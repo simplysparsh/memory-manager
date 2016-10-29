e32370894f63df738390bdcedb37272b5c6e4ab2
Sparsh Saxena <simplysparsh.s@gmail.com>
2016-10-29 15:35:14 -0400

    add initial project setup

---
+++ src/my_memory.c
@@ -0,0 +1,51 @@
+/*****************************************************************
+ *
+ * File             : my_memory.c
+ * Description      :
+ *
+ * Author           : @author(s)
+ * Last Modified    : @date
+ *
+**/
+// Include files
+#include <stdio.h>
+#include <stdlib.h>
+#include "memalloc.h"
+
+void setup( int malloc_type, int mem_size, void* start_of_memory ) {
+/**
+ *  Fill your code here
+ *
+**/
+}
+
+void *my_malloc(int size) {
+/**
+ *  Fill your code here
+ *
+**/
+	return (void*)-1;
+}
+
+void my_free(void *ptr) {
+/**
+ *  Fill your code here
+ *
+**/
+}
+
+int num_free_bytes() {
+/**
+ *  Fill your code here
+ *
+**/
+    return 0;
+}
+
+int num_holes() {
+/**
+ *  Fill your code here
+ *
+**/
+    return 0;
+}
