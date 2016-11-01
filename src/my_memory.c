eb273d04f6f3858b14a1a617ca89c88ad07c6ad7
Sparsh Saxena <simplysparsh.s@gmail.com>
2016-11-01 15:39:13 -0400

    Initialize global variables

--- src/my_memory.c
+++ src/my_memory.c
@@ -3,28 +3,52 @@
  * File             : my_memory.c
  * Description      :
  *
- * Author           : @author(s)
- * Last Modified    : @date
+ * Author           : Sparsh Saxena
+ * Last Modified    : November 8th, 2016
  *
-**/
+******************************************************************/
+
 // Include files
 #include <stdio.h>
 #include <stdlib.h>
 #include "memalloc.h"
 
+// Global Variables
+int malloc_type = -1;
+int mem_size = 0;
+void* start_of_memory = NULL;
+void* end_of_memory = NULL; 
+
 void setup( int malloc_type, int mem_size, void* start_of_memory ) {
-/**
- *  Fill your code here
- *
-**/
+
+    malloc_type = malloc_type;
+    mem_size = mem_size;
+    start_of_memory = start_of_memory; 
+    end_of_memory = start_of_memory + mem_size;
 }
 
 void *my_malloc(int size) {
-/**
- *  Fill your code here
- *
-**/
-	return (void*)-1;
+
+    switch (malloc_type) {
+
+        case FIRST_FIT:
+            //code
+            break;
+
+        case BEST_FIT:
+            //code 
+            break;
+
+        case WORST_FIT:
+            // code
+            break;
+
+        case BUDDY_SYSTEM:
+            // code
+            break;
+    }
+
+    return (void*)-1;
 }
 
 void my_free(void *ptr) {
